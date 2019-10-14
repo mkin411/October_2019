4fd71eb3c552aa0424454538f553abfb647411e2
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-10-14 07:55:26 -0500

    updated

--- Exam2.R
+++ Exam2.R
@@ -1,5 +1,18 @@
 #Exam 2
 
+fd<-load("fish_data (10).Rdata")
+fds.<-fish;rm(fish)
+fs<-fds.[,c("transect.id", "parcel.id", "parcel.density.m3", "parcel.length.m", "area_fac", "depth_fac")]
+head(fs)
 
+
+
+library(stringr)
+t<-read.table(file='ISIIS201405281124.txt', sep="\t", skip=10, header = TRUE, fileEncoding = "ISO-8859-1", stringsAsFactors = FALSE, quote = "\"", check.names = FALSE, encoding = "UTF-8", na.strings = "9999.99")
+date<-scan(file = 'ISIIS201405281124.txt', what = "character",skip = 1, nlines = 1, quiet = TRUE)
+head(t)
+
+
+library(readxl)
 
 
